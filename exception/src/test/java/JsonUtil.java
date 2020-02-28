3ca4fd40928097344e271d6e34f802f5078e58b1
Edward <edwardlin.zlt@gmail.com>
2020-02-28 11:40:07 +0800

    Done!

--- exception/src/test/java/JsonUtil.java
+++ exception/src/test/java/JsonUtil.java
@@ -1,15 +1,23 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class JsonUtil {
 
-  private JsonUtil() {
-  }
-  private static final ObjectMapper objectMapper = new ObjectMapper();
+    private JsonUtil() {
+    }
 
-  public static String convertToJson(Object object) {
-    //TODO: change the code to pass the test
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    throw new NotImplementedException();
-  }
+    public static String convertToJson(Object object) {
+        //TODO: change the code to pass the test
+
+        String s;
+        try {
+            s = objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new ValueReadException("Read value error");
+        }
+        return s;
+    }
 }
--- exception/src/test/java/ValueReadException.java
+++ exception/src/test/java/ValueReadException.java
@@ -5,13 +5,11 @@ public class ValueReadException extends RuntimeException {
   public ValueReadException(String message) {
 
     //TODO: change the code to pass the test
-
-    throw new NotImplementedException();
+      super(message);
   }
 
   public ValueReadException(String message, Throwable cause) {
     //TODO: change the code to pass the test
-
-    throw new NotImplementedException();
+      super(message, cause);
   }
 }
